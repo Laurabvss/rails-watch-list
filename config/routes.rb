0f1e500c6d3cc895641bf59e5cb977b7c26031d9
Laura Beauvais <lauralisa.beauvais@gmail.com>
2023-08-17 18:31:42 +0200

    work in progress

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  #get 'lists', to: 'lists#index', as: :lists
+  #get 'lists/new', to: 'lists#new', as: :new_list
+  #get 'lists/:id', to: 'lists#show', as: :list
+  #post 'lists', to: 'lists#create'
+
+  #get 'lists/:id/bookmarks/new', to: 'bookmarks#new', as: :new_bookmark
+  #post 'lists/:id/bookmarks', to: 'bookmarks#create'
+
+  resources :lists, only: [:index, :new, :create, :show] do
+    member do
+      get 'lists/:id/bookmarks/new', to: 'bookmarks#new', as: :new_bookmark
+      post 'lists/:id/bookmarks', to: 'bookmarks#create'
+    end
+    resources :bookmarks, only: %i[new create]
+  end
+  root to: 'lists#index'
+  delete 'bookmarks/:id', to: 'bookmarks#destroy'
 end
